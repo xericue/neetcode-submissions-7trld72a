9add58b55057642e0f495af292b26009f5369ffd
xericue <106630482+xericue@users.noreply.github.com>
2026-04-01 08:09:45 -0400

    Add: two-integer-sum-ii - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        # since this is already sorted we're gonna want to use two pointers
+        
+        # return the indexes
+        # cannot use the same index twice
+
+        # since its increasing/already sorted
+        # if the given answer is greater than target:
+            # move right down
+        # if the given answer is less than target:
+            # move left up
+
+        left, right = 0, len(numbers) - 1
+
+        while left < right:
+            if numbers[left] + numbers[right] == target:
+                return [left + 1, right + 1]
+            
+            elif numbers[left] + numbers[right] > target:
+                right -= 1
+            
+            elif numbers[left] + numbers[right] < target:
+                left += 1
